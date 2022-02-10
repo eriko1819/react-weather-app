3ed798e1470f2c8add7b69ce566187a9a5f3dad1
Eriko <eriko.918k@gmail.com>
2022-02-10 10:32:39 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+
+      
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
-        This project was coded by me is{" "} 
+        This project was coded by me is{" "}
         <a
           href="https://github.com/eriko1819/react-weather-app"
           target="_blank"
-        > open-sourced on GitHub</a>
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
       </footer>
+      </div>
     </div>
   );
 }
