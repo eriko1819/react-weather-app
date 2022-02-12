cf1bd145760e05a6e546b764dccdad3e1c595798
Eriko <eriko.918k@gmail.com>
2022-02-12 21:55:32 +0900

    Basic Styling

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div className="Weather"> ???
       <form>
         <div className="row">
           <div className="col-9">
@@ -15,9 +15,11 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" 
-              value="Search" 
-              className="btn btn-primary w-100" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -29,17 +31,19 @@ export default function Weather() {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
+            <div className="float-left">
             <img
-             src="https://ssl.gstatic.com/onebox/weather/48/snow_light.png"
-             alt="Snow Showers"
-             className="float-left"
+              src="https://ssl.gstatic.com/onebox/weather/48/snow_light.png"
+              alt="Snow Showers"
+              className="float-left"
             />
+            </div>
+          </div>
             <div className="float-left">
               <span className="temp">2</span>
-              <span className="units">°C</span>
+              <span className="units">°C</span>  
             </div>
-          </div> 
-        </div>
+        </div>   
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
@@ -47,7 +51,8 @@ export default function Weather() {
             <li>Wind: 13 km/h</li>
           </ul>
         </div>
+
       </div>
-    </div>
+      </div>
   );
 }
